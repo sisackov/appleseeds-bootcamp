822179a8b3c62426c2f7387a09048d19134fef1d
sisackov <1123889+sisackov@users.noreply.github.com>
2021-11-30 10:36:07 +0200

    udemy js section 10 form events

--- udemy/javascript/communicating-with-events/14_09_Form_Events/app.js
+++ udemy/javascript/communicating-with-events/14_09_Form_Events/app.js
@@ -4,11 +4,13 @@ const creditCardInput = document.querySelector('#cc');
 const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
-form.addEventListener('submit', function(e) {
-	e.preventDefault(); //stops the request from being sent (prevents page reload)
-	console.log('cc', creditCardInput.value);
-	console.log('terms', termsCheckbox.checked);
-	console.log('veggieSelect', veggieSelect.value);
-	//send form data to db
-	//append something to page using form data
+form.addEventListener('submit', function (e) {
+    //stops the request from being sent (prevents page reload) which is the default behavior of a form submit
+    e.preventDefault();
+
+    console.log('cc', creditCardInput.value);
+    console.log('terms', termsCheckbox.checked);
+    console.log('veggieSelect', veggieSelect.value);
+    //send form data to db
+    //append something to page using form data
 });
