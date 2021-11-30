ddf055e1be92792a411b4901ce0216188e1be093
sisackov <1123889+sisackov@users.noreply.github.com>
2021-11-30 15:37:38 +0200

    js 21-3 done

--- javascript/week-5-6/prevent-default-and-form-events/22-3/main.js
+++ javascript/week-5-6/prevent-default-and-form-events/22-3/main.js
@@ -6,10 +6,11 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 let answer = randomLetterSelector();
 console.log(answer);
 let guessesArray = [];
+const containerElement = document.querySelector('.container');
 const inputMessageElement = document.querySelector('.input-message');
 const guessedKeysElement = document.querySelector('.guessed-keys');
-// console.dir(inputMessageElement);
-// console.dir(guessedKeysElement);
+const reloadMessage = document.querySelector('.reload-message');
+const reloadButton = document.querySelector('.reload-button');
 
 // **************************************************
 // Define functions
@@ -34,35 +35,49 @@ function handleUserInput(input) {
         input = input.toLowerCase();
         console.log('handleUserInput', input);
         if (answer === input) {
-            handleCorrectGuess();
+            handleCorrectGuess(input);
         } else {
             handleWrongGuess(input);
         }
     }
 }
 
-function handleCorrectGuess() {
-    clearClassList(inputMessageElement);
-    inputMessageElement.classList.add('input-message--correct');
-    inputMessageElement.textContent = 'Right letter!';
-    guessedKeysElement.textContent = '';
-    answer = randomLetterSelector();
-    guessesArray = [];
+function displayMessage(message, color) {
+    inputMessageElement.textContent = message;
+    inputMessageElement.style.color = color;
 }
 
 function handleWrongGuess(input) {
-    inputMessageElement.classList.add('input-message--incorrect');
-    console.log(handleWrongGuess);
-    console.dir(inputMessageElement);
-    console.dir(guessedKeysElement);
+    inputMessageElement.style.color = 'red';
     if (guessesArray.includes(input)) {
-        inputMessageElement.textContent = input + ' has already been guessed';
+        displayMessage(input + ' has already been guessed', 'red');
     } else {
         guessesArray.push(input);
-        console.log(guessesArray);
-        inputMessageElement.textContent = 'Nope, wrong letter';
+        displayMessage('Nope, wrong letter', 'red');
         guessedKeysElement.textContent = guessesArray.join(', ');
     }
+}
+
+function handleCorrectGuess(input) {
+    displayMessage(input + ' - Right letter!', 'green');
+    changeVisibilityOfBottomElements(false);
+}
+
+function changeVisibilityOfBottomElements(visibility) {
+    document.querySelector('h2').style.display = visibility ? 'block' : 'none';
+    guessedKeysElement.style.display = visibility ? 'block' : 'none';
+
+    reloadMessage.style.display = visibility ? 'none' : 'block';
+    reloadButton.style.display = visibility ? 'none' : 'block';
+}
+
+function resetGame() {
+    answer = randomLetterSelector();
+    guessesArray = [];
+
+    displayMessage('Guess a letter', 'black');
+    guessedKeysElement.textContent = '';
+    changeVisibilityOfBottomElements(true);
 }
 
 function clearClassList(element) {
@@ -75,17 +90,11 @@ function clearClassList(element) {
 // Define event listeners
 // **************************************************
 
-// reset the input message when the user starts typing
-// window.addEventListener('beforeinput', () => {
-//     console.log('beforeinput');
-//     clearClassList(inputMessageElement);
-//     inputMessageElement.textContent = 'Guess a letter';
-//     console.log(inputMessageElement);
-// });
-
 window.addEventListener('keyup', (event) => {
     const input = event.key;
     console.log(event);
     console.log(input);
     handleUserInput(input);
 });
+
+reloadButton.addEventListener('click', () => resetGame());
