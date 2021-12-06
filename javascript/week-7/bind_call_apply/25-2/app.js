9fbf79413249ed674f3a75694066c8ed5ea96a5a
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-06 13:02:03 +0200

    js 25-3  call apply

--- javascript/week-7/bind_call_apply/25-2/app.js
+++ javascript/week-7/bind_call_apply/25-2/app.js
@@ -1,13 +1,3 @@
-/**
-*?  1.  Q - What is wrong with the code ? explain in your own words
-**      A - The code is not working because the this in getStrength points to the global object.
-**          The function whoIsStronger is not bound to the object.
-**  2. Fix the code so that invoking the whoIsStronger function will print "I am stronger".
-*       !note: you cannot change the 'hero' or the whoIsStronger function.
-
-
-*/
-
 const person = {
     name: 'Yoni',
     print1: function () {
