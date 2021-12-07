a7cd471f8c64d7b6008c2fa51aeced347ab0009d
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-07 12:17:17 +0200

    js 28-1  ajax

---
+++ javascript/week-7/ajax/28-1/app.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body');
+const button = document.createElement('button');
+button.innerText = 'Show me a joke';
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
+body.appendChild(button);
+body.appendChild(h1);
+body.appendChild(p);
+
+function fetchJoke() {
+    fetch('https://api.jokes.one/jod')
+        .then(response => response.json())
+        .then(data => {
+            const joke = data.contents.jokes[0].joke;
+            console.table(joke);
+            h1.innerText = joke.title;
+            p.innerText = joke.text;
+        }).catch(error => {
+            console.log(error);
+        });
+}
+
+button.addEventListener('click', fetchJoke);
+
