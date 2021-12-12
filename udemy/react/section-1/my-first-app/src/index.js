e270e28b8069845e2213a30efc02d4092b8fb232
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-12 14:15:01 +0200

    udemy react section 1 done

--- udemy/react/section-1/my-first-app/src/index.js
+++ udemy/react/section-1/my-first-app/src/index.js
@@ -1,17 +1,15 @@
+// Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
+if (module.hot) {
+    module.hot.accept();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return <div>Hi there! fff</div>;
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
