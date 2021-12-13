9f22f45c91290b67dc7f010d66d494c4ffee32dd
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-13 14:01:11 +0200

    udemy react section 5 done

---
+++ udemy/react/section-5/57-initiallizing/seasons/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  // React says we have to define render!!
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ udemy/react/section-5/60-handling-errors/seasons/src/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setstate!!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
