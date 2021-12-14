211fbc74226f8487c346f40fabfe06a971f7dac5
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-14 14:25:36 +0200

    react 8.1 done

--- react/week-8/lifecycle-methods/8-1-lifecycle-methods/src/index.js
+++ react/week-8/lifecycle-methods/8-1-lifecycle-methods/src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//hot replacement
+if (module.hot) {
+    module.hot.accept();
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { favoriteColor: 'blue' };
+        this.updatedElem = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        setTimeout(() => {
+            this.setState({ favoriteColor: 'red' });
+        }, 1500);
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+        // document.getElementById('updatedDiv').innerText = 'componentDidUpdate';//the vanilla js way
+        this.updatedElem.current.innerText = `The updated favorite color is ${this.state.favoriteColor}`;
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <h1>{`My favorite color is ${this.state.favoriteColor}`}</h1>
+                <div id='updatedDiv' ref={this.updatedElem}></div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
