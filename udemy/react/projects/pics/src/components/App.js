fb48f320cbb393054fdcdae7e6ecbc5184c453d1
sisackov <1123889+sisackov@users.noreply.github.com>
2021-12-15 21:20:00 +0200

    udemy react section 10 done

--- udemy/react/projects/pics/src/components/App.js
+++ udemy/react/projects/pics/src/components/App.js
@@ -4,24 +4,24 @@ import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
-  state = { images: [] };
+    state = { images: [] };
 
-  onSearchSubmit = async term => {
-    const response = await unsplash.get('/search/photos', {
-      params: { query: term }
-    });
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },
+        });
 
-    this.setState({ images: response.data.results });
-  };
+        this.setState({ images: response.data.results });
+    };
 
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='ui container' style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
+            </div>
+        );
+    }
 }
 
 export default App;
