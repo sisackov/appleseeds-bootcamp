5cb8671c64d93620cfdd1f7992b468ed648fe9cd
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-10 16:59:06 +0200

    udemy node section 6-36 done

--- udemy/nodejs/code-alongs/weather-app/app.js
+++ udemy/nodejs/code-alongs/weather-app/app.js
@@ -1,11 +1,42 @@
-console.log('Starting')
+const request = require('postman-request');
+const geocode = require('./utils/geocode');
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+// const url =
+//     'http://api.weatherstack.com/current?access_key=57544947ac948939da9e83f838c8dfd6&query=Jerusalem';
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!');
+//     } else if (response.body.error) {
+//         console.log('Unable to find location');
+//         console.error(response.body.error.info);
+//     } else {
+//         const data = JSON.parse(response.body);
+//         // console.log(data.current);
+//         console.log(
+//             `It is currently ${data.current.temperature}${String.fromCharCode(
+//                 176
+//             )} degrees out. There is a ${data.current.precip}% chance of rain.`
+//         );
+//     }
+// });
 
-console.log('Stopping')
+// const geocodeURL =
+//     'https://api.mapbox.com/geocoding/v5/mapbox.places/Beer%20Sheba.json?access_token=pk.eyJ1IjoiY2ZzZHZzZCIsImEiOiJja3k4cjhlczUxaHlkMnhvajRtMTFxcTQ5In0.cPp1Fl6CEptNTTgLjIhbjw&limit=1';
+
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to location services!');
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location. Try another search.');
+//     } else {
+//         const latitude = response.body.features[0].center[0];
+//         const longitude = response.body.features[0].center[1];
+//         console.log(latitude, longitude);
+//     }
+// });
+
+geocode('Beer Sheba', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+});
