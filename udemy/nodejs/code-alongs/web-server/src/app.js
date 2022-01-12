be890c3b914700d1302cf9b6cc76d0912317f412
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-12 08:23:29 +0200

    udemy node section 7 done

--- udemy/nodejs/code-alongs/web-server/src/app.js
+++ udemy/nodejs/code-alongs/web-server/src/app.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
-//allows to set a setting for the server to use.
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -25,6 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead',
     });
 });
 
@@ -32,6 +43,22 @@ app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
         location: 'Philadelphia',
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.',
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.',
     });
 });
 
