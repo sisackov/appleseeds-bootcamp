893e25e9378baa13708779a0bdee82df4274263f
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-20 01:58:21 +0200

    getPlayerData works

--- projects/webcrawler/puppeteer.js
+++ projects/webcrawler/puppeteer.js
@@ -40,6 +40,15 @@ const qbTableHeaders = [
     'Fumbles Lost',
 ];
 
+async function getTableRows(page, selector) {
+    return page.$$eval(selector, (trs) =>
+        trs.map((tr) => {
+            const tds = [...tr.querySelectorAll('td')];
+            return tds.map((td) => td.textContent);
+        })
+    );
+}
+
 async function getPlayerStats(playerName) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -78,3 +87,110 @@ async function getPlayerStats(playerName) {
 }
 
 // getPlayerStats('tom-brady');
+const neededOffensivePositions = ['QB', 'RB', 'WR', 'TE'];
+const playerDataTableHeader = [
+    'imageLink',
+    'name',
+    'position',
+    'age',
+    'height',
+    'weight',
+    'experience',
+    'college',
+];
+
+const teamNamesEspn = [
+    'buf/buffalo-bills',
+    'mia/miami-dolphins',
+    'ne/new-england-patriots',
+    'nyj/new-york-jets',
+    'bal/baltimore-ravens',
+    'cin/cincinnati-bengals',
+    'cle/cleveland-browns',
+    'pit/pittsburgh-steelers',
+    'hou/houston-texans',
+    'ind/indianapolis-colts',
+    'jax/jacksonville-jaguars',
+    'ten/tennessee-titans',
+    'den/denver-broncos',
+    'kc/kansas-city-chiefs',
+    'oak/oakland-raiders',
+    'lac/los-angeles-chargers',
+    'chi/chicago-bears',
+    'det/detroit-lions',
+    'gb/green-bay-packers',
+    'min/minnesota-vikings',
+    'dal/dallas-cowboys',
+    'nyg/new-york-giants',
+    'phi/philadelphia-eagles',
+    'wsh/washington',
+    'atl/atlanta-falcons',
+    'car/carolina-panthers',
+    'no/new-orleans-saints',
+    'tb/tampa-bay-buccaneers',
+    'ari/arizona-cardinals',
+    'sf/san-francisco-49ers',
+    'sea/seattle-seahawks',
+    'lar/los-angeles-rams',
+];
+async function getPlayersNameAndPosition(teamName) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Instructs the blank page to navigate a URL
+    await page.goto(`https://www.espn.com/nfl/team/roster/_/name/${teamName}`);
+
+    // Waits until the `title` meta element is rendered
+    await page.waitForSelector('title');
+
+    const selector = 'div.ResponsiveTable.Offense table > tbody > tr';
+    const tableRows = await page.$$eval(selector, (trs) =>
+        trs.map((tr) => {
+            const tds = [...tr.querySelectorAll('td')];
+            return tds.map((td, index) => {
+                try {
+                    if (index === 0) {
+                        return td.querySelector('img').getAttribute('alt');
+                    } else if (index === 1) {
+                        return `${td.querySelector('a').textContent}__${
+                            td.querySelector('span').textContent
+                        }`;
+                    }
+                    return td.textContent;
+                } catch (err) {
+                    console.log(err);
+                }
+                return '';
+            });
+        })
+    );
+
+    // console.log(tableRows);
+
+    const data = tableRows
+        .map((row) => {
+            const rowData = {};
+            row.forEach((cell, index) => {
+                if (index === 0) {
+                    rowData[playerDataTableHeader[index]] = cell;
+                    rowData.espnId = cell
+                        .slice(cell.lastIndexOf('/') + 1)
+                        .split('.')[0];
+                } else if (index === 1) {
+                    const split = cell.split('__');
+                    rowData[playerDataTableHeader[index]] = split[0];
+                    rowData.number = split[1];
+                    rowData.teamName = teamName.split('/')[1];
+                } else {
+                    rowData[playerDataTableHeader[index]] = cell;
+                }
+            });
+            return rowData;
+        })
+        .filter((player) => neededOffensivePositions.includes(player.position));
+
+    console.log(data); //Works!!!
+
+    // await browser.close();
+}
+getPlayersNameAndPosition(teamNamesEspn[31]);
