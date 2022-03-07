a5787b736552b7305b3973324b71cfa6c3450ce2
sisackov <1123889+sisackov@users.noreply.github.com>
2022-03-07 21:50:34 +0200

    project vanilla js

---
+++ projects/js-tests/main.js
@@ -0,0 +1,20 @@
+// import {
+//     initialMatrix,
+//     initGameConstants,
+//     resourceMap,
+//     createGridElement,
+//     GRID_ROWS,
+//     GRID_COLS,
+//     getCurrentMatrix,
+//     setCurrentMatrix,
+//     RESOURCE,
+//     TREE_COUNT
+// } from './game.js';
+
+// const gameGrid = document.querySelector('.game-container');
+// const inventoryToolGrid = document.querySelector('.ic__tool-container');
+// const inventoryResourceGrid = document.querySelector('.ic__resource-container');
+// const resetButton = document.querySelector('#reset-button');
+// const randomButton = document.querySelector('#random-button');
+// const saveButton = document.querySelector('#save-button');
+// const loadButton = document.querySelector('#load-button');
